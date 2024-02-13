43dc9afb1b9362ea35a1315d9ec961214d154e57
Ezequiel Mascotena <ezequielmascotena@gmail.com>
2024-02-12 21:57:58 -0300

    maqueta del chat desde el homeroute

--- src/routes/home.route.js
+++ src/routes/home.route.js
@@ -1,25 +1,42 @@
 const { Router } = require('express');
-const ProductManager = require('../dao/fileSystem/ProductManager');
+//FS
+//const ProductManager = require('../dao/fileSystem/ProductManager');
+//const productManager = new ProductManager('./products.json')
 
-const productManager = new ProductManager('./products.json')
+//Mongoose
+const ProductManagerMongo = require('../dao/db/managers/ProductManager');
+const productManager= new ProductManagerMongo();
 
 const router = new Router()
 
 async function obtenerProductos() {
-    return await productManager.getProducts();
+    try{
+        return await productManager.getProducts();
+    } catch (err){
+        return `error al obtener Productos: ${err}`
+    }
 }
 
 router.get('/', async (req, res) => {
-    const products = await obtenerProductos();
-    const datos = { products };
-    res.render('home.handlebars', datos)
+    try{
+        const products = await obtenerProductos();
+        const datos = { products };
+        res.render('home.handlebars', datos)
+    } catch (err) {
+        return `error al obtener Productos: ${err}`
+    }
 })
 
-//con socket
+//productos con socket
 router.get('/realtimeproducts', async (req, res) => {
     const products = await obtenerProductos();
     const datos = { products };
     res.render('realTimeProducts.handlebars', datos)
+})
+
+//chat con socket
+router.get('/chat', async (req, res)=>{
+    res.render('chat.handlebars', {})
 })
 
 
