371676b11790c2ffe700fbc9c23f6b42e1f40a02
Ezequiel Mascotena <ezequielmascotena@gmail.com>
2024-02-24 01:53:07 -0300

    corregí un detalle del chat y separé la logica socket del server

--- src/app.js
+++ src/app.js
@@ -4,14 +4,12 @@ const handlebars = require('express-handlebars');
 const routerHome = require('./routes/home.route.js');
 const routerProd = require('./routes/products.route.js');
 const routerCart = require('./routes/carts.route.js');
+
 const http = require('http')
+
+//Socket
 const { Server } = require('socket.io')
-
-const ProductManagerMongo = require('../src/dao/db/managers/ProductManager.js');
-const productManager = new ProductManagerMongo();
-
-const ChatManagerMongo = require('../src/dao/db/managers/ChatManager');
-const chatManager = new ChatManagerMongo();
+const socketService = require ('./socket/index.js')
 
 const PORT = 8080 || process.env.PORT
 const app = express()
@@ -36,45 +34,14 @@ app.use('/api', routerHome)
 app.use('/api/products', routerProd)
 app.use('/api/carts', routerCart)
 
-//Socket Server
+//Socket
 const io = new Server(server)
-io.on('connection', (socket) => {
-    console.log('New User Connected')
-
-    //productos
-    socket.on('addProdInfo', async (data) => {
-        try {
-            await productManager.addProduct(data);
-            const updatedProducts = await productManager.getProducts();
-            socket.emit('prodsToRender', updatedProducts);
-        } catch (error) {
-            console.error('Error al agregar producto:', error);
-        }
-    });
-
-    socket.on('deleteProdInfo', async (data) => {
-        try {
-            await productManager.deleteProduct(data.id);
-            const updatedProducts = await productManager.getProducts();
-            socket.emit('prodsToRender', updatedProducts);
-        } catch (error) {
-            console.error('Error al eliminar producto:', error);
-        }
-    });
-
-    //chat
-    socket.on('newMenssage', async (msg) => {
-        try {
-            await chatManager.addMsg(msg)
-            const chat = await chatManager.getChat()
-            socket.emit('allMsgs', chat)
-        } catch (err) {
-            console.error('Error al leer los msjs:', error);
-        }
-    })
-})
+io.on ('connection', (socket)=> socketService(socket, io))
 
 server.listen(PORT, () => {
     console.log(`Server run on port ${PORT}`)
     Database.connect()
 })
+
+
+module.exports = app 
---
+++ src/socket/index.js
@@ -0,0 +1,45 @@
+
+
+
+const ProductManagerMongo = require('../dao/db/managers/ProductManager');
+const productManager = new ProductManagerMongo();
+
+const ChatManagerMongo = require('../dao/db/managers/ChatManager');
+const chatManager = new ChatManagerMongo();
+
+
+//Socket Server
+const socketService = (socket, io) => {
+    console.log('New User Connected')
+
+    //productos
+    socket.on('addProdInfo', async (data) => {
+        try {
+            await productManager.addProduct(data);
+            const updatedProducts = await productManager.getProducts();
+            socket.emit('prodsToRender', updatedProducts);
+        } catch (error) {
+            console.error('Error al agregar producto:', error);
+        }
+    });
+
+    socket.on('deleteProdInfo', async (data) => {
+        try {
+            await productManager.deleteProduct(data.id);
+            const updatedProducts = await productManager.getProducts();
+            socket.emit('prodsToRender', updatedProducts);
+        } catch (error) {
+            console.error('Error al eliminar producto:', error);
+        }
+    });
+
+
+    //chat
+    socket.on('newMenssage', async (msg) => {
+        await chatManager.addMsg(msg)
+        const chat = await chatManager.getChat()
+        io.sockets.emit('allMsgs', chat)
+    })
+}
+
+module.exports = socketService
